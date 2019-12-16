5fe30642c8b8466611c1f5c0af26055bff7ef3bb
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-16 17:30:19 +0100

    Single Linked List

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 #include "Linked_Lists.h"
 
+#define N 10000
 int main() {
-    int T[] = {1, 2, 3, 4, 5, 6};
-    setList_t* pList = NULL;
-    do_create_linked_list(createLinkedList, &pList, 1);
-    do_create_linked_list(createLinkedList, &pList, 2);
+    node_t* firstNode = NULL;
+    clock_t start = clock();
+    // I will start my linked list with value N and decrement it until I achieve 1
+    for (int i = 0; i < N; ++i) {
+        do_create_node(create_node, &firstNode, N-i);
+    }
+    // test the program if he can achieve the last point 1
+    uint32_t data = findData(firstNode, 1); // it returns 0 if data was not in the list
+    free(firstNode);
+    clock_t end = clock();
+    // Calculate the time spent to do all these operations
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("time spent: %.2f\ndata found = %d", time_spent, data);
     return 0;
 }
